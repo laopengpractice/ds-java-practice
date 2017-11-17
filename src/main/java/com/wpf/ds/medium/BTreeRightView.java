fc0eb756e98ae9e5c7dc5a3f4f88e35204198857
laopengpractice <13521658765@139.com>
2017-11-17 20:01:53 +0800

    ADD:

---
+++ src/main/java/com/wpf/ds/medium/BTreeRightView.java
@@ -0,0 +1,46 @@
+package com.wpf.ds.medium;
+
+import com.wpf.ds.common.PrintUtils;
+import com.wpf.ds.common.TreeNode;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by wenpengfei on 2017/11/17.
+ */
+public class BTreeRightView {
+
+    private static Set<Integer> rightSideSet = new HashSet<>();
+    private static List<Integer> rs = new ArrayList<>();
+
+    public static List<Integer> rightSizeViewDfs(TreeNode root) {
+        dfs(root, 1);
+
+        return rs;
+    }
+
+    private static void dfs(TreeNode root, int level) {
+        if (root == null) {
+            return;
+        }
+
+        if (!rightSideSet.contains(level)) {
+            rs.add(root.val);
+            rightSideSet.add(level);
+        }
+        dfs(root.right, level + 1);
+        dfs(root.left, level + 1);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.left.right.left = new TreeNode(6);
+        PrintUtils.printList(rightSizeViewDfs(root));
+    }
+}
---
+++ src/main/java/com/wpf/ds/medium/EvalDivision.java
@@ -0,0 +1,11 @@
+package com.wpf.ds.medium;
+
+/**
+ * Created by wenpengfei on 2017/11/17.
+ */
+public class EvalDivision {
+
+    public double[] calcEq(String[][] eqs, double[] vals, String[][] queries) {
+        return null;
+    }
+}
