4951be64561267cae9a78381c6fb492c95833bc7
laopengpractice <13521658765@139.com>
2017-11-22 20:32:17 +0800

    ADD

---
+++ src/main/java/com/wpf/ds/medium/KthNum.java
@@ -0,0 +1,58 @@
+package com.wpf.ds.medium;
+
+/**
+ * Created by wenpengfei on 2017/11/22.
+ */
+public class KthNum {
+
+    public static int kthNum1(int[] nums, int k) {
+        return help(nums, 0, nums.length - 1, k);
+    }
+
+    private static int help(int[] nums, int b, int e, int k) {
+        if (b == e) {
+            return nums[b];
+        }
+
+        int p = nums[e];
+        int m = b - 1;
+        int n = e;
+        for (int i = b; i < n; ) {
+            if (nums[i] < p) {
+                m++;
+                if (m != i) {
+                    swap(nums, i, m);
+                }
+                i++;
+            } else if (nums[i] > p) {
+                n--;
+                if (n != i) {
+                    swap(nums, i, n);
+                }
+            }
+        }
+
+        if (e != n) {
+            swap(nums, e, n);
+            n++;
+        }
+
+        if (m - b + 1 >= k) {
+            return help(nums, b, m, k);
+        } else if (k > n - b) {
+            return help(nums, n, e, k - n - b);
+        }
+
+        return p;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(kthNum1(new int[]{5, 2, 3, 4, 1, 10, 7, 9}, 6));
+    }
+}
